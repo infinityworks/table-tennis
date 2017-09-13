90f4cdae408388d22faba38fd2a0731124331976
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-09-13 20:52:11 +0100

    Added and fixed up Unit Tests for Ladder

--- TestLadder.py
+++ TestLadder.py
@@ -1,10 +1,21 @@
 import unittest
+from ladder import Table_Tennis_Ladder
 
 class TestTableTennis(unittest.TestCase):
-			
-	def test_file_parser():
-		file_path = 'names.txt'
-		table_tennis = ladder
-		parsed_names_list = parse_names_file(file_path)
 
-		self.assertEqual(['dan', 'ivor', 'rebecca', 'john', 'bob', 'rowan'], parsed_names_list)
+	def test_file_parser_Should_return_list_with_names(self):
+		file_path = 'names.txt'
+		ladder = Table_Tennis_Ladder()
+		parsed_names_list = ladder.parse_names_file(file_path)
+		self.assertEqual(['dan', 'ivor', 'rebecca', 'rowan', 'john', 'bob'], parsed_names_list)
+			
+	# def test_file_parser_Should_return_list_with_names(self):
+	# 	file_path = 'names.txt'
+	# 	ladder = Table_Tennis_Ladder()
+	# 	parsed_names_list = ladder.parse_names_file(file_path)
+
+	# 	self.assertEqual(['dan', 'ivor', 'rebecca', 'john', 'bob', 'rowan'], parsed_names_list.toString())
+
+
+if __name__ == '__main__':
+    unittest.main()
--- ladder.py
+++ ladder.py
@@ -1,6 +1,15 @@
-file = 'names.txt'
+class Table_Tennis_Ladder:
 
-def parse_names_file(path_to_file):
-	input_file = open(path_to_file, 'r')
-	for names in path_to_file:
-		array_for_writing.append(names.strip())
+	file = 'names.txt'
+
+	def __init__(self):
+		pass
+
+	def parse_names_file(self,path_to_file):
+		players_list = []
+		input_file = open(path_to_file, 'r')
+		for names in input_file:
+			players_list.append(names.strip())
+		return players_list
+
+	
