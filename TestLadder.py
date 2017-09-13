d90764d167c06d8cc458c32c695baa3c3fa7d83a
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-09-13 20:58:36 +0100

    Updated TestLadder.py

--- TestLadder.py
+++ TestLadder.py
@@ -9,12 +9,6 @@ class TestTableTennis(unittest.TestCase):
 		parsed_names_list = ladder.parse_names_file(file_path)
 		self.assertEqual(['dan', 'ivor', 'rebecca', 'rowan', 'john', 'bob'], parsed_names_list)
 			
-	# def test_file_parser_Should_return_list_with_names(self):
-	# 	file_path = 'names.txt'
-	# 	ladder = Table_Tennis_Ladder()
-	# 	parsed_names_list = ladder.parse_names_file(file_path)
-
-	# 	self.assertEqual(['dan', 'ivor', 'rebecca', 'john', 'bob', 'rowan'], parsed_names_list.toString())
 
 
 if __name__ == '__main__':
