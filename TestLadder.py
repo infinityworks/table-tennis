385ff8bca76543111bbbfdb902c91ff024712ea8
Ivor Cheung <ivor.cheung@infinityworks.com>
2017-09-14 14:03:30 +0100

    Added read method and unit test. Started working on other methods to ladder.py

--- TestLadder.py
+++ TestLadder.py
@@ -6,10 +6,21 @@ class TestTableTennis(unittest.TestCase):
 	def test_file_parser_Should_return_list_with_names(self):
 		file_path = 'names.txt'
 		ladder = Table_Tennis_Ladder()
-		parsed_names_list = ladder.parse_names_file(file_path)
+		parsed_names_list = ladder.parse_names_from_file(file_path)
 		self.assertEqual(['dan', 'ivor', 'rebecca', 'rowan', 'john', 'bob'], parsed_names_list)
 			
+	def test_parsed_file_Should_write_to_file(self):
+		file_path = 'test_write.txt'
+		names_list = ['dan', 'ivor', 'rebecca', 'rowan', 'john', 'bob']
+		ladder = Table_Tennis_Ladder()
+		new_names_list = ladder.write_names_from_list_to_file(file_path, names_list)
 
+		input_file = open(file_path, 'r')
+		return_names_list = []
+		for names in input_file:
+			return_names_list.append(names)
+
+		self.assertEqual(['dan\n', 'ivor\n', 'rebecca\n', 'rowan\n', 'john\n', 'bob\n'], return_names_list)
 
 if __name__ == '__main__':
     unittest.main()
--- ladder.py
+++ ladder.py
@@ -2,14 +2,27 @@ class Table_Tennis_Ladder:
 
 	file = 'names.txt'
 
-	def __init__(self):
-		pass
-
-	def parse_names_file(self,path_to_file):
-		players_list = []
+	def parse_names_from_file(self,path_to_file):
+		self.players_list = []
 		input_file = open(path_to_file, 'r')
 		for names in input_file:
 			players_list.append(names.strip())
 		return players_list
 
-	
+	def write_names_from_list_to_file(self,path_to_file,names_list):
+		output_file = open(path_to_file, 'w')
+
+		# each player in reordered list added back into 
+		for name in names_list:
+			output_file.write("{}\n".format(name))
+
+	def new_list_of_players(self, player_list, player_a, player_b):
+		top  = player_list[:player_b]
+		winner = player_list[player_a]
+		middle = player_list[player_b:player_a]
+		bottom = player_list[player_a + 1:]
+
+		# make winner a list instead of a string then concatenate the four lists
+		new_player_list = top + [winner] + middle + botto
+
+		return new_player_list
